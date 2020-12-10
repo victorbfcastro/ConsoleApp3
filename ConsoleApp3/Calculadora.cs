a9616a3e5c9bed27a23ad3d7d1cd039172d6a833
ricanascimento <ricardo.paulino.nascimento@prestadoresuhg.com.br>
2020-12-10 11:48:15 -0300

    Add project files.

---
+++ ConsoleApp3/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace Projeto
+{
+    public class Calculadora
+    {
+        // Soma(double n1, double n2)
+        public double Soma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Soma(params double[] numeros)
+        {
+            double resultado = 0.0;
+            foreach (double numero in numeros)
+            {
+                resultado += numero;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto
+{  
+    class Program
+    { 
+        public static void Main(String[] args)
+        {
+            Calculadora calc;
+            calc = new Calculadora();
+
+            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
+
+            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = calc.Soma(n1, n2);
+            Console.WriteLine("resultado 1 =" + resultado);
+
+
+            double resultado2 = calc.Soma(numeros);
+            Console.WriteLine("resultado 2 =" + resultado2);
+
+
+            Console.Read();
+        }
+    }
+
+}
