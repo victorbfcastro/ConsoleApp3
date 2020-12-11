bb0b92c4dc61c7af6234b30ea5b7a1c62c055182
victorbfcastro <victorbfc@hotmail.com>
2020-12-11 18:20:28 -0300

    Calculadora Final

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,20 +1,186 @@
-﻿namespace Projeto
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projeto
 {
     public class Calculadora
     {
-        // Soma(double n1, double n2)
-        public double Soma(double n1, double n2)
+        private List<double> listaNumeros = new List<double>();
+
+        public List<double> RetornaLista(string operacao)      //Método para ler os números a serem calculados
         {
-            return n1 + n2;
+            string tipoOperacao = operacao;
+
+            Console.WriteLine($"\n[{tipoOperacao}] Informe os números a serem calculados (Digite '=' para calcular): \n");
+            string n = Console.ReadLine();
+            TestaNumero(tipoOperacao, n);
+
+            return listaNumeros;
         }
-        public double Soma(params double[] numeros)
+
+        private string TestaNumero(string operacao, string n)
         {
+            while (n != "=")
+            {
+                double testeNum = 0;
+
+                if (double.TryParse(n, out testeNum) && n.Length != 0)   // Teste se o valor digitado é numero e se está vazio
+                {
+                    if (operacao == "Dividir")
+                    {
+                        while (n == "0")
+                        {
+                            Console.WriteLine("Não é possível dividir por zero! Tente outro valor: ");
+                            n = Console.ReadLine();
+                        }
+                        listaNumeros.Add(double.Parse(n));
+                        n = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Informe um número válido!");
+                    n = Console.ReadLine();
+                }
+
+
+            }
+
+            return n;
+        }
+
+        public void Somar()
+        {
+            List<double> listaSomar = RetornaLista("Somar");
+
             double resultado = 0.0;
-            foreach (double numero in numeros)
+
+            foreach (double numero in listaSomar)
             {
                 resultado += numero;
             }
-            return resultado;
+
+            Console.WriteLine($"{resultado}\n");
+            ResetaLista();
+        }
+
+        public void Subtrair()
+        {
+            List<double> listaSubtrair = RetornaLista("Subtrair");
+
+            double resultado = 0.0;
+
+            foreach (double numero in listaSubtrair)
+            {
+                if (numero == listaSubtrair[0])     /* Verifica se é o primeiro valor digitado e 
+                                                       começa a subtrair a partir dele. */
+                {
+                    resultado = numero;
+                }
+                else
+                {
+                    resultado -= numero;
+                }
+
+            }
+
+            Console.WriteLine($"{resultado}\n");
+            ResetaLista();
+        }
+
+        public void Multiplicar()
+        {
+            List<double> listaMultiplicar = RetornaLista("Multiplicar");
+
+            double resultado = 0.0;
+            foreach (double numero in listaMultiplicar)
+            {
+                if (numero == listaMultiplicar[0])      /* Verifica se é o primeiro valor digitado e 
+                                                           começa a multiplicar a partir dele. */
+                {
+                    resultado = numero;
+                }
+                else
+                {
+                    resultado *= numero;
+                }
+            }
+            Console.WriteLine($"{resultado}\n");
+            ResetaLista();
+
+        }
+
+        public void Dividir()
+        {
+            List<double> listaDividir = RetornaLista("Dividir");
+
+            double resultado = 0.0;
+
+            foreach (double numero in listaDividir)
+            {
+                for (int i = 0; i < listaDividir.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        resultado = listaDividir[0];
+                    }
+                    else
+                    {
+                        resultado /= listaDividir[i];
+                    }
+
+                }
+                Console.WriteLine($"{resultado}\n");
+                break;
+            }
+            ResetaLista();
+        }
+
+        public void ResetaLista()           /* Zera a lista de números para não acumular 
+                                               valores antigos com as novas operacoes */
+        {
+            listaNumeros.Clear();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine(">> Menu Calculadora");
+            Console.WriteLine("\n" +
+                "[1] Soma\n" +
+                "[2] Subtrair\n" +
+                "[3] Multiplicar\n" +
+                "[4] Dividir\n" +
+                "[0] Sair\n");
+            Console.Write("Opção: ");
+            string resposta = Console.ReadLine();
+
+            while (true)
+            {
+                switch (resposta)
+                {
+                    case "1":
+                        Somar();
+                        break;
+
+                    case "2":
+                        Subtrair();
+                        break;
+
+                    case "3":
+                        Multiplicar();
+                        break;
+
+                    case "4":
+                        Dividir();
+                        break;
+
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                }
+                Menu();
+            }
+
         }
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,30 +1,19 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace Projeto
-{  
+{
     class Program
-    { 
+    {
         public static void Main(String[] args)
         {
-            Calculadora calc;
-            calc = new Calculadora();
+            Calculadora calc = new Calculadora();
 
-            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
-
-            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double resultado = calc.Soma(n1, n2);
-            Console.WriteLine("resultado 1 = " + resultado);
-
-
-            double resultado2 = calc.Soma(numeros);
-            Console.WriteLine("resultado 2 =" + resultado2);
-
-
+            calc.Menu();
             Console.Read();
         }
+
     }
 
 }
